b8f2e435fa373569b936a5eaf0d783af1bbb5f1d
13401731 <a13401731@mac13401731L.local>
2022-04-05 13:48:31 +0100

    First commit

---
+++ src/main/java/com/example/encryption/EntityConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityConverterApplication.class, args);
+	}
+
+}
